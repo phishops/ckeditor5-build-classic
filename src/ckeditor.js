092e6dcabbfdf516df52f3568c17488dc06598bd
Jake Sylvestre <jakesyl@gmail.com>
2019-03-25 00:43:18 -0400

    kill default mergefields

--- src/ckeditor.js
+++ src/ckeditor.js
@@ -99,13 +99,5 @@ ClassicEditor.defaultConfig = {
 		]
 	},
 	// This value must be kept in sync with the language defined in webpack.config.js.
-	language: 'en',
-	mergeFields: {
-		options: [
-			{
-				displayValue: 'First Name',
-				name: 'firstName'
-			}
-		]
-	}
+	language: 'en'
 };
